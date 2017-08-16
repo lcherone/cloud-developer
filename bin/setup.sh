e182e18852caf07a7cecb18f668735d247687ab6
Lawrence Cherone <lawrence@cherone.co.uk>
2017-08-16 17:18:33 +0000

    Tweek setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,70 +12,8 @@ warn() {
     whiptail --title "$TITLE" --msgbox "You must complete setup or the script will not work." 0 0
 }
 
-main() {
+write_config() {
     #
-    whiptail --title "$TITLE" --msgbox "Please hit OK to complete setup." 0 0
-    
-    # Check for wkhtmltopdf
-    if [ $(dpkg-query -W -f='${Status}' wkhtmltopdf 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        whiptail --title "$TITLE" --msgbox "wkhtmltopdf needs to be installed." 0 0
-        whiptail --title "$TITLE" --infobox "Installing package: wkhtmltopdf" 0 0
-        sudo apt-get install -yq wkhtmltopdf
-    fi
-    
-    # Check for libfontconfig1
-    if [ $(dpkg-query -W -f='${Status}' libfontconfig1 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        whiptail --title "$TITLE" --msgbox "libfontconfig1 needs to be installed." 0 0
-        whiptail --title "$TITLE" --infobox "Installing package: libfontconfig1" 0 0
-        sudo apt-get install -yq libfontconfig1
-    fi
-        
-    # Check for libxrender1
-    if [ $(dpkg-query -W -f='${Status}' libxrender1 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        whiptail --title "$TITLE" --msgbox "libxrender1 needs to be installed." 0 0
-        whiptail --title "$TITLE" --infobox "Installing package: libxrender1" 0 0
-        apt-get install -yq libxrender1
-    fi
-    
-    # Database Host
-    DBHOST=$(whiptail --inputbox "Enter database host:" 8 70 "127.0.0.1" --title "$TITLE" 3>&1 1>&2 2>&3)
-
-    exitstatus=$?
-    if [ $exitstatus = 1 ]; then
-        warn
-    fi
-    
-    # Database Database
-    DBNAME=$(whiptail --inputbox "Enter database name:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
-
-    exitstatus=$?
-    if [ $exitstatus = 1 ]; then
-        warn
-    fi
-    
-    # Database User
-    DBUSER=$(whiptail --inputbox "Enter database username:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
-
-    exitstatus=$?
-    if [ $exitstatus = 1 ]; then
-        warn
-    fi
-    
-    # Database Password
-    DBPASS=$(whiptail --inputbox "Enter database password:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
-
-    exitstatus=$?
-    if [ $exitstatus = 1 ]; then
-        warn
-    fi
-    
-    #whiptail --title "$TITLE" --infobox "Generating plinker private key." 0 0
-    PLINKER_KEY=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-
-    #whiptail --title "$TITLE" --infobox "Writing ./app/config.ini file." 0 0
     echo -e "[globals]
 
 meta.name=\"Cloud Developer\"
@@ -83,15 +21,16 @@ meta.description=\"\"
 meta.author=\"Lawrence Cherone\"
 
 ; database
-db.dsn=\"mysql:host=$DBHOST;dbname=$DBNAME\"
-db.username=\"$DBUSER\"
-db.password=\"$DBPASS\"
+db.host=\"$1\"
+db.name=\"$2\"
+db.username=\"$3\"
+db.password=\"$4\"
 db.freeze=false
 db.debug=false
 
 ; plinker
 plinker.enabled=true
-plinker.private_key=\"$PLINKER_KEY\"
+plinker.private_key=\"$5\"
 
 ; framework
 BASE=
@@ -99,11 +38,10 @@ CACHE=false
 DEBUG=3
 AUTOLOAD=\"app/\"
 
-" > $PWD/app/config.ini
+" > $6/app/config.ini
+}
 
-    #sleep 1
-
-    #whiptail --title "$TITLE" --infobox "Writing ./bin/backup.sh file." 0 0
+write_backup_script() {
     echo -e "#!/bin/bash
 
 #
@@ -115,10 +53,10 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 export DISPLAY=:0.0
 
 # Database credentials
-user=\"$DBUSER\"
-password=\"$DBPASS\"
-host=\"$DBHOST\"
-db_name=\"$DBNAME\"
+db_host=\"$1\"
+db_name=\"$2\"
+db_user=\"$3\"
+db_pass=\"$4\"
 
 # Set paths ect
 date=\$(date +\"%d-%b-%Y\")
@@ -129,35 +67,75 @@ date=\$(date +\"%d-%b-%Y\")
     # - Start database dump
     #
     # In cron we are doing:
-    # */5 * * * * cd $PWD/bin && bash backup.sh
+    # */5 * * * * cd $5/bin && bash backup.sh
     #
     # With the idea.. in development backup every 5 mins, with the latest 
     #                 and then copy it to the daily if does not exist.
     
-    mysqldump --user=\$user --password=\$password --host=\$host \$db_name | gzip > $PWD/backups/current.sql.gz
+    mysqldump --user=\$user --password=\$password --host=\$host \$db_name | gzip > $5/backups/current.sql.gz
 
     # Check if already backed up today
-    if [ ! -f $PWD/backups/\$date.sql.gz ]; then
+    if [ ! -f $5/backups/\$date.sql.gz ]; then
         # Dump database into SQL file
-        cp $PWD/backups/current.sql.gz $PWD/backups/\$date.sql.gz
+        cp $5/backups/current.sql.gz $5/backups/\$date.sql.gz
     fi
 
     # Delete files older than 7 days
-    find $PWD/backups/*.sql.gz -mtime +7 -exec rm {} \;
+    find $5/backups/*.sql.gz -mtime +7 -exec rm {} \;
 
     ##############################################################
     
 } &> /dev/null
-" > $PWD/bin/backup.sh
+" > $5/bin/backup.sh
+}
 
+main() {
     #
+    whiptail --title "$TITLE" --msgbox "Please hit OK to complete setup." 0 0
+
+    # Database Host
+    DBHOST=$(whiptail --inputbox "Enter database host:" 8 70 "127.0.0.1" --title "$TITLE" 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 1 ]; then
+        warn
+    fi
+    
+    # Database Database
+    DBNAME=$(whiptail --inputbox "Enter database name:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 1 ]; then
+        warn
+    fi
+    
+    # Database User
+    DBUSER=$(whiptail --inputbox "Enter database username:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 1 ]; then
+        warn
+    fi
+    
+    # Database Password
+    DBPASS=$(whiptail --inputbox "Enter database password:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 1 ]; then
+        warn
+    fi
+    
+    #whiptail --title "$TITLE" --infobox "Generating plinker private key." 0 0
+    PLINKER_KEY=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+
+    #whiptail --title "$TITLE" --infobox "Writing ./app/config.ini file." 0 0
+    write_config $DBHOST $DBNAME $DBUSER $DBPASS $PLINKER_KEY $PWD
+
+    #whiptail --title "$TITLE" --infobox "Writing ./bin/backup.sh file." 0 0
+    write_backup_script $DBHOST $DBNAME $DBUSER $DBPASS $PWD
+
     #whiptail --title "$TITLE" --infobox "Adding CRON task" 0 0
     crontab -l | { cat; echo "\n* * * * * cd $PWD/tasks && /usr/bin/php $PWD/tasks/run.php >/dev/null 2>&1"; } | crontab -
-    crontab -l | { cat; echo "\n*/5 * * * * cd $PWD/bin && bash backup.sh"; } | crontab -
+    crontab -l | { cat; echo "\n0 0 * * * cd $PWD/bin && bash backup.sh"; } | crontab -
     
-    #sleep 1
+    # fin
     whiptail --title "$TITLE" --msgbox "Setup complete!" 0 0
-
 }
 
 main
