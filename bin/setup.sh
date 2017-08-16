c0c20706eafe28bce42f79e629cace7854dade25
Lawrence Cherone <lawrence@cherone.co.uk>
2017-08-16 18:07:41 +0000

    Fix setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -38,7 +38,7 @@ CACHE=false
 DEBUG=3
 AUTOLOAD=\"app/\"
 
-" > $6/app/config.ini
+" > $PWD/app/config.ini
 }
 
 write_backup_script() {
@@ -67,26 +67,26 @@ date=\$(date +\"%d-%b-%Y\")
     # - Start database dump
     #
     # In cron we are doing:
-    # */5 * * * * cd $5/bin && bash backup.sh
+    # */5 * * * * cd $PWD/bin && bash backup.sh
     #
     # With the idea.. in development backup every 5 mins, with the latest 
     #                 and then copy it to the daily if does not exist.
     
-    mysqldump --user=\$user --password=\$password --host=\$host \$db_name | gzip > $5/backups/current.sql.gz
+    mysqldump --user=\$user --password=\$password --host=\$host \$db_name | gzip > $PWD/backups/current.sql.gz
 
     # Check if already backed up today
-    if [ ! -f $5/backups/\$date.sql.gz ]; then
+    if [ ! -f $PWD/backups/\$date.sql.gz ]; then
         # Dump database into SQL file
-        cp $5/backups/current.sql.gz $5/backups/\$date.sql.gz
+        cp $PWD/backups/current.sql.gz $PWD/backups/\$date.sql.gz
     fi
 
     # Delete files older than 7 days
-    find $5/backups/*.sql.gz -mtime +7 -exec rm {} \;
+    find $PWD/backups/*.sql.gz -mtime +7 -exec rm {} \;
 
     ##############################################################
     
 } &> /dev/null
-" > $5/bin/backup.sh
+" > $PWD/bin/backup.sh
 }
 
 main() {
@@ -94,48 +94,56 @@ main() {
     whiptail --title "$TITLE" --msgbox "Please hit OK to complete setup." 0 0
 
     # Database Host
-    DBHOST=$(whiptail --inputbox "Enter database host:" 8 70 "127.0.0.1" --title "$TITLE" 3>&1 1>&2 2>&3)
-    exitstatus=$?
-    if [ $exitstatus = 1 ]; then
-        warn
-    fi
+    until [[ $DBHOST != "" ]]; do
+        DBHOST=$(whiptail --inputbox "Enter database host:" 8 70 "127.0.0.1" --title "$TITLE" 3>&1 1>&2 2>&3)
+        exitstatus=$?
+        if [ $exitstatus = 1 ]; then
+            warn
+        fi
+    done
     
     # Database Database
-    DBNAME=$(whiptail --inputbox "Enter database name:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
-    exitstatus=$?
-    if [ $exitstatus = 1 ]; then
-        warn
-    fi
+    until [[ $DBNAME != "" ]]; do
+        DBNAME=$(whiptail --inputbox "Enter database name:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
+        exitstatus=$?
+        if [ $exitstatus = 1 ]; then
+            warn
+        fi
+    done
     
     # Database User
-    DBUSER=$(whiptail --inputbox "Enter database username:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
-    exitstatus=$?
-    if [ $exitstatus = 1 ]; then
-        warn
-    fi
-    
+    until [[ $DBUSER != "" ]]; do
+        DBUSER=$(whiptail --inputbox "Enter database username:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
+        exitstatus=$?
+        if [ $exitstatus = 1 ]; then
+            warn
+        fi
+    done
+
     # Database Password
-    DBPASS=$(whiptail --inputbox "Enter database password:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
-    exitstatus=$?
-    if [ $exitstatus = 1 ]; then
-        warn
-    fi
+    until [[ $DBPASS != "" ]]; do
+        DBPASS=$(whiptail --inputbox "Enter database password:" 8 70 "" --title "$TITLE" 3>&1 1>&2 2>&3)
+        exitstatus=$?
+        if [ $exitstatus = 1 ]; then
+            warn
+        fi
+    done
     
     #whiptail --title "$TITLE" --infobox "Generating plinker private key." 0 0
     PLINKER_KEY=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 
     #whiptail --title "$TITLE" --infobox "Writing ./app/config.ini file." 0 0
-    write_config $DBHOST $DBNAME $DBUSER $DBPASS $PLINKER_KEY $PWD
+    write_config $DBHOST $DBNAME $DBUSER $DBPASS $PLINKER_KEY
 
     #whiptail --title "$TITLE" --infobox "Writing ./bin/backup.sh file." 0 0
-    write_backup_script $DBHOST $DBNAME $DBUSER $DBPASS $PWD
+    write_backup_script $DBHOST $DBNAME $DBUSER $DBPASS
 
     #whiptail --title "$TITLE" --infobox "Adding CRON task" 0 0
     crontab -l | { cat; echo "\n* * * * * cd $PWD/tasks && /usr/bin/php $PWD/tasks/run.php >/dev/null 2>&1"; } | crontab -
     crontab -l | { cat; echo "\n0 0 * * * cd $PWD/bin && bash backup.sh"; } | crontab -
     
     # fin
-    whiptail --title "$TITLE" --msgbox "Setup complete!" 0 0
+    whiptail --title "$TITLE" --msgbox "Setup complete! Visit the script in your browser." 0 0
 }
 
 main
